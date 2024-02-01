7597aed07ef3b9061375b0cb14a871402827f397
Chen, Wei-Yu <49446811+WilliamChen1109@users.noreply.github.com>
2024-02-01 19:47:31 +0800

    Update SwerveDriveJoystickCmd.java

--- SwerveDriveJoystickCmd.java
+++ SwerveDriveJoystickCmd.java
@@ -3,7 +3,10 @@ package frc.robot.commands;
 import java.util.function.Supplier;
 
 import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.DriveConstants;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class SwerveDriveJoystickCmd extends Command{
@@ -41,11 +44,24 @@ public class SwerveDriveJoystickCmd extends Command{
         double x = x_spd.get();
         double rot = rot_spd.get();
         
+        y = Math.abs(y) < 0.01 ? 0 : y;
+        x = Math.abs(x) < 0.01 ? 0 : x;
+        rot = Math.abs(rot) < 0.01 ? 0 : rot;
+
         y = yLimiter.calculate(y);
         x = xLimiter.calculate(x);
         rot = rotLimiter.calculate(rot);
 
+        ChassisSpeeds chassisSpeeds;
+        if(fieldOriented.get()){
+            chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(x, y, rot, swerveSubsystem.getRotation2d());
+        }
+        else{
+            chassisSpeeds = new ChassisSpeeds(x, y, rot);
+        }
 
+        SwerveModuleState[] swerveModuleStates = DriveConstants.kSwerveKinematics.toSwerveModuleStates(chassisSpeeds);
+        swerveSubsystem.setModuleStates(swerveModuleStates);
     }
 
     @Override
